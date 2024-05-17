3a906b939e788ee76cc100eb10d82908abc515b6
Simonova <mazukata@gmail.com>
2024-05-17 17:02:53 +0300

    hw02-annotation-config - поменяла аннотацию

--- hw02-annotation-config/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,9 +2,9 @@ package ru.otus.hw.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @Data
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
