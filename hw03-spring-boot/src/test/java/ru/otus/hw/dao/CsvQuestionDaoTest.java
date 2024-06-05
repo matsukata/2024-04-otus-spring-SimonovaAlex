07cc2f3cc210c48d30149a00471e8270695194b1
Simonova <mazukata@gmail.com>
2024-06-05 08:30:02 +0300

    hw03-spring-boot - поправила тест

--- hw03-spring-boot/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw03-spring-boot/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -3,8 +3,10 @@ package ru.otus.hw.dao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.service.TestRunnerServiceImpl;
 
 import java.util.List;
 
@@ -12,6 +14,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CsvQuestionDaoTest {
+    @MockBean
+    private TestRunnerServiceImpl testRunnerService;
+
     @Autowired
     private CsvQuestionDao csvQuestionDao;
 
