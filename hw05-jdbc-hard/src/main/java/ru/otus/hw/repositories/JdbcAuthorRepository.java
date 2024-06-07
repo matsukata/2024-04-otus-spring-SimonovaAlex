3d7bf9f06a0782bc6066ca6f3c37c58508618a13
Simonova <mazukata@gmail.com>
2024-06-07 06:13:55 +0300

    hw05-jdbc - начала дополнять репозиторий

--- hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
+++ hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -1,17 +1,20 @@
 package ru.otus.hw.repositories;
 
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Author;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Repository
+@AllArgsConstructor
 public class JdbcAuthorRepository implements AuthorRepository {
+    private final NamedParameterJdbcOperations namedParameterJdbcOperations;
 
     @Override
     public List<Author> findAll() {
@@ -20,14 +23,22 @@ public class JdbcAuthorRepository implements AuthorRepository {
 
     @Override
     public Optional<Author> findById(long id) {
-        return Optional.empty();
+        Map<String, Object> params = Collections.singletonMap("id", id);
+        Author author = namedParameterJdbcOperations.queryForObject(
+                "select id, full_name from authors where id = :id", params, new AuthorRowMapper()
+        );
+        return author == null ? Optional.empty() : Optional.of(author);
     }
 
     private static class AuthorRowMapper implements RowMapper<Author> {
 
-        @Override
-        public Author mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            @Override
+            public Author mapRow(ResultSet resultSet, int i) throws SQLException {
+                long id = resultSet.getLong("id");
+                String name = resultSet.getString("full_name");
+                return new Author(id, name);
+            }
         }
     }
+    
 }
