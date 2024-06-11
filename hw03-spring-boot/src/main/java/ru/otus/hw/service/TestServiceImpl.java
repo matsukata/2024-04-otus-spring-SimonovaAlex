61f0f61ec0528becc0f2e7059c79b3bc4310efa5
Simonova <mazukata@gmail.com>
2024-06-11 09:08:15 +0300

    Revert "hw03-spring-boot - пытаюсь поправить чекстайл"
    This reverts commit 983e8ad1a41f14090022e7d3a1637772682ac441.

--- hw03-spring-boot/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -23,7 +23,6 @@ public class TestServiceImpl implements TestService {
 
     @Override
     public TestResult executeTestFor(Student student) {
-
         ioService.printLine("");
         ioService.printLineLocalized("TestService.answer.the.questions");
         ioService.printLine("");
