dd350a3e3f6fdd201acb7805ae6952d5146429a5
Simonova <mazukata@gmail.com>
2024-06-11 09:08:40 +0300

    Revert "hw03-spring-boot - домашка готова"
    This reverts commit 818f7a4abfd827e82a0437a30d7be3ef6ec9c08b.

--- hw03-spring-boot/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw03-spring-boot/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -3,6 +3,7 @@ package ru.otus.hw.dao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Question;
 
@@ -12,6 +13,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CsvQuestionDaoTest {
+    @Autowired
+    private AppProperties fileNameProvider;
+
     @Autowired
     private CsvQuestionDao csvQuestionDao;
 
